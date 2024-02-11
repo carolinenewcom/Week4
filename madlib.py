e4d213ce00f9d74e8701efe45689ff63153f5c74
carolinenewcom <carolinenewcom33@gmail.com>
2024-02-10 18:52:56 -0600

    commit changes

---
+++ madlib.py
@@ -0,0 +1,13 @@
+print ("Madlib Maker")
+rOne= input("name: ")
+rTwo= input("noun: ")
+rThree= input("color: ")
+rFour= input("noun: ")
+rFive= input("action verb: ")
+rSix= input("action verb: ")
+
+print("There once was a person named",rOne,"that lived in a",rTwo)
+print(rOne,"had long",rThree,"hair")
+print("The",rTwo,"was the size of a",rFour)
+print(rOne,"loved to",rFive,"and",rSix,"in the",rTwo)
+print(rOne,"lived happily ever after in the",rTwo)
